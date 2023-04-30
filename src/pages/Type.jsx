5e3cbbd82cf55c9a4b844aa25963e94d2a661d88
leedahye <leedahye2001@naver.com>
2023-04-30 23:38:01 +0900

    refactor: componentize pages containing keywords using props

--- src/pages/Type.jsx
+++ src/pages/Type.jsx
@@ -1,7 +1,4 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { Block, YearWrapper } from "../css/YearStyle";
-
+import Keyword from "../components/Keyword";
 
 const Type = () => {
   const type = [
@@ -25,47 +22,13 @@ const Type = () => {
     },
   ];
 
+  const test = [
+    { keyword: "본인이 선호하는 콘텐츠(영상 종류)를 선택해주세요." },
+  ];
+
   return (
     <>
-      <div>
-        <div className="flex grid items-center">
-          <div className="relative bg-black justify-center align-center h-[1024px]"></div>
-          <YearWrapper>
-            <h1>본인이 선호하는 콘텐츠(영상 종류)를 선택해주세요.</h1>
-            {/* <Block>
-              <YearContent> */}
-            <div className="grid">
-              {type.map(({ keyword }) => (
-                <button
-                  className="grid items-center bg-[#303030] mt-10 rounded-md h-[60px] hover:bg-gray-500
-                  hover:ring-2 ring-orange-500"
-                  key={keyword}
-                >
-                  <h2 className="text-white grid align-center">{keyword}</h2>
-                </button>
-              ))}
-            </div>
-            <div className="flex justify-between">
-              <button
-                className="grid items-center bg-[#fff] mt-10 rounded-md h-[60px] 
-                  hover:ring-2 ring-orange-500 w-[100px]"
-              >
-                <h2 className="text-[#f57b00] grid align-center">모르겠어요</h2>
-              </button>
-              <Link to="/year">
-                <button
-                  className="grid items-center bg-[#f57b00] mt-10 rounded-md h-[60px]
-                  hover:ring-2 ring-white w-[100px]"
-                >
-                  <h2 className="text-white grid align-center">다음</h2>
-                </button>
-              </Link>
-            </div>
-            {/* </YearContent>
-            </Block> */}
-          </YearWrapper>
-        </div>
-      </div>
+      <Keyword type={type} test={test} />
     </>
   );
 };
